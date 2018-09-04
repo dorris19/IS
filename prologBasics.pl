930ab297e90ff9d199a02a369a0f192ac7df30d3
Dylan Orris <dorris19@wooster.edu>
2018-09-03 21:33:17 -0400

    prolog testing file

---
+++ prologBasics.pl
@@ -0,0 +1,14 @@
+mortal(X) :- human(X).
+fallible(X) :- human(X).
+immortal(X) :- god(X).
+
+demigod(X) :- god(X), human(X).
+
+falseIdol(X) :- god(X), fallible(X).
+
+
+human(joe).
+human(hercules).
+god(hercules).
+god(zeus).
+
