fb73d080260ffadce02c424b71708ea487db6ef8
Dylan Orris <dorris19@wooster.edu>
2018-09-16 22:19:47 -0400

    forgot to add Perl implementation for parser import

---
+++ perlParser.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+use AI::Prolog;
+use File::Slurp;
+use Data::Dumper;
+use v5.16;
+
+#Take user input for question to be proved
+say 'What would you like to test?';
+my $test_string = <STDIN>;
+chomp $test_string;
+
+#Read in parser file to put rules in
+my $database = read_file('parser.pl');
+
+#define prolog based on our read in database
+my $prolog = AI::Prolog->new($database);
+
+#Would usually be $test_string, but for testing currently is as defined
+$prolog->query("prove that the third angle of a triangle is mathStuff.");
+
+#puts out answer
+while (my $result = $prolog->results) {
+	print Dumper $result;
+}
+#if what we had was a valid question, we work with it
+#Will probably need a new database to do determinations of what axioms to use
+if($result eq 'True.'){
+	
+}
