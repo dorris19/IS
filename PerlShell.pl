426a6f8d663f6a746bc4c05e849c23f2a06560e1
Dylan Orris <dorris19@wooster.edu>
2018-09-25 09:49:29 -0400

    pass through on input claim now transmits to prolog and outputs proper result

--- PerlShell.pl
+++ PerlShell.pl
@@ -3,7 +3,20 @@ use strict;
 use IO::File;
 use v5.16;
 
-my $returncode = system("swipl -g 'consult('parser')' -g 'recognize_topdown([that,the,third,angle,of,a,triangle,is,mathStuff])' -t halt 2> /dev/null");
+
+say 'What would you like to test?';
+my $test_string = <STDIN>;
+chomp $test_string;
+my @test_list = split(' ', $test_string);
+
+my $pass_through = join(',', @test_list);
+
+#my $returncode = system("swipl -g 'consult('parser')' -g 'recognize_topdown([that,the,third,angle,of,a,triangle,is,mathStuff])' -t halt 2> /dev/null");
+my $first_string = qq{swipl -g 'consult('parser')' -g 'recognize_topdown([};
+my $second_string = qq{])' -t halt 2> /dev/null};
+my $string_test = join "",$first_string, $pass_through, $second_string;
+
+my $returncode = system($string_test);
 
 
 if($returncode == 0){
