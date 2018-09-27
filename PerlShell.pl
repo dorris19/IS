f159f5279f3da58c7f79ff753996641be8da1e08
Dylan Orris <dorris19@wooster.edu>
2018-09-26 22:01:40 -0400

    style changes - small steps, last style change led to inoperable code?

--- PerlShell.pl
+++ PerlShell.pl
@@ -3,18 +3,26 @@ use strict;
 use IO::File;
 use v5.16;
 
-
+#Take user input, remove formatting, and split to an array
 say 'What would you like to test?';
 my $test_string = <STDIN>;
 chomp $test_string;
 my @test_list = split(' ', $test_string);
 
+#Turn array into a string for passing to prolog
+
 my $pass_through = join(',', @test_list);
 
-#my $returncode = system("swipl -g 'consult('parser')' -g 'recognize_topdown([that,the,third,angle,of,a,triangle,is,mathStuff])' -t halt 2> /dev/null");
+#Format string for shell use
+
 my $first_string = qq{swipl -g 'consult('parser')' -g 'recognize_topdown([};
 my $second_string = qq{])' -t halt 2> /dev/null};
 my $string_test = join "",$first_string, $pass_through, $second_string;
+
+
+#Run command, if no errors are produced, statement is true.
+#If errors do occur, prolog either found something wrong with the input
+#or it returned false - we assume false here
 
 my $returncode = system($string_test);
 
